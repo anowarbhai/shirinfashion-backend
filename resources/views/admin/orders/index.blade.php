04a069fd57ff8baa70cb4c8489572761afcc3eae
Anowar <anowar@example.com>
2026-04-12 09:55:44 +0600

    Fix extra closing div causing syntax error

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -176,7 +176,6 @@ function formatCurrencyAdmin($amount, $symbol, $position) {
             @if($orders->isEmpty())
             <div class="p-8 text-center text-gray-500">No orders found</div>
             @endif
-        </div>
     </div>
     
     @if($orders->hasPages())
